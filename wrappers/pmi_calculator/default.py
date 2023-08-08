c5894ddf0db9d2dd3992bd42ed4d12521d3236b5
Aaron Chen <chenaaron316@gmail.com>
2023-08-08 07:04:26 +0000

    Built wrapper function for pmi_calculator service

---
+++ wrappers/pmi_calculator/default.py
@@ -0,0 +1,57 @@
+from wrappers import register_wrapper
+from wrappers.base import BaseWrapper, BaseResponse
+from pydantic import BaseModel
+from typing import List, Dict
+
+class PmiCalculatorInput(BaseModel):
+    tree: List[Dict]
+
+
+class PmiCalculatorResult(BaseModel):
+    __root__: list[float]
+
+class PmiCalculatorOutput(BaseModel):
+    error: str
+    status: str
+    results: list[PmiCalculatorResult]
+
+class PmiCalculatorResponse(BaseResponse):
+    result: list[PmiCalculatorResult]
+
+@register_wrapper(
+    name="pmi_calculator",
+    input_class=PmiCalculatorInput,
+    output_class=PmiCalculatorOutput,
+    response_class=PmiCalculatorResponse
+)
+class PmiCalculatorWrapper(BaseWrapper):
+    """Wrapper class for Pmi Calculator"""
+    prefixes = ["pmi_calculator"]
+
+    def call_sync(self, input: PmiCalculatorInput) -> PmiCalculatorResponse:
+        # print('+++++++++++++++++++++++++++++')
+        # print('output')
+        # print(super().call_sync(input=input))
+        output = super().call_raw(input=input)
+        response = self.convert_output_to_response(output)
+        # return super().call_sync(input=input)
+        return response
+
+    async def call_async(self, input: PmiCalculatorInput, priority: int = 0) -> str:
+        # Edied by Aaron Chen
+        return await super().call_async(input=input, priority=priority)
+
+    async def retrieve(self, task_id: str) -> PmiCalculatorResponse | None:
+        return await super().retrieve(task_id=task_id)
+
+    @staticmethod
+    def convert_output_to_response(output: PmiCalculatorOutput
+                                   ) -> PmiCalculatorResponse:
+        response = {
+            "status_code": 200,
+            "message": "",
+            "result": output.results
+        }
+        response = PmiCalculatorResponse(**response)
+
+        return response
