14780d3b8a14a5076377d5ccc664f4daf3b2c5a0
Zhengkai Tu <ztu@mit.edu>
2024-04-18 14:08:49 -0400

    register shell functions

--- deploy.sh
+++ deploy.sh
@@ -538,9 +538,9 @@ else
   for arg in "$@"
   do
     case "$arg" in
-      clean-data | start-db-services | save-db | seed-db | copy-nginx-conf | pull-images | \
+      clean-data | start-db-services | save-db | seed-db | copy-nginx-conf | pull-images | generate-deployment-scripts | \
       start-web-services | start-ml-servers | start-celery-workers | set-db-defaults | count-mongo-docs | \
-      backup | restore | index-db | diff-env | post-update-message | old-messages )
+      backup | restore | index-db | diff-env | post-update-message | old-messages | get-images)
         # This is a defined function, so execute it
         $arg
         ;;
