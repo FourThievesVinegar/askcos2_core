04c5d1ab9347cba50cd9059114d7953ef52ad10d
Fong Mun Hong <fongmh1947@gmail.com>
2024-04-19 12:13:57 -0400

    buyables sim search aggregate

--- utils/similarity_search_utils.py
+++ utils/similarity_search_utils.py
@@ -262,6 +262,155 @@ def sim_search_aggregate(
 
     return response
 
+def sim_search_buyables(mol, mol_collection, count_collection=None, threshold=DEFAULT_THRESHOLD
+               ) -> list:
+    """
+    Searches `mol_collection` for molecules with Tanimoto similarity to `mol`
+    greater than or equal to `threshold`.
+    Args:
+        mol (rdmol Object): query molecule
+        mol_collection (pymongo Collection): collection that has precomputed
+            Morgan Fingerprints and bit counts.
+        count_collection (pymongo Collection): popularity counts for Morgan
+            fingerprint bits for all entries in `mol_collection`
+        threshold: Tanimoto threshold for similarity
+    Returns
+        A list of MongoDB documents that fulfill `threshold`, including their
+        tanimoto scores.
+    """
+    results = []
+    qfp = list(
+        AllChem.GetMorganFingerprintAsBitVect(
+            mol, DEFAULT_MORGAN_RADIUS, nBits=DEFAULT_MORGAN_LEN
+        ).GetOnBits()
+    )
+
+    if threshold == 0:
+        for mol in mol_collection.find({}):
+            tanimoto = calc_tanimoto(qfp, mol["mfp"]["bits"])
+            mol["tanimoto"] = tanimoto
+            results.append(mol)
+        return results
+
+    qfp_count = len(qfp)
+    fp_min = int(math.ceil((threshold * qfp_count)))
+
+    try:
+        fp_max = int(qfp_count / threshold)
+        print("adn here")
+    except ZeroDivisionError:
+        fp_max = float("inf")
+    print("made it here")
+    req_common_count = qfp_count - fp_min + 1
+    if count_collection:
+        req_common_bits = [
+            count["_id"]
+            for count in count_collection.find({"_id": {"$in": qfp}})
+            .sort("count", 1)
+            .limit(req_common_count)
+        ]
+        print(req_common_bits)
+    else:
+        req_common_bits = qfp[:req_common_count]
+    for mol in mol_collection.find(
+        {
+            "mfp.count": {"$gte": fp_min, "$lte": fp_max},
+            "mfp.bits": {"$in": req_common_bits},
+        }
+    ):
+        tanimoto = calc_tanimoto(qfp, mol["mfp"]["bits"])
+        mol["tanimoto"] = tanimoto
+        if tanimoto >= threshold:
+            results.append(mol)
+
+    return results
+
+
+def sim_search_aggregate_buyables(
+    mol, mol_collection, count_collection=None, threshold=DEFAULT_THRESHOLD
+) -> list:
+    """
+    Searches `mol_collection` for molecules with Tanimoto similarity to `mol`
+    greater than or equal to `threshold`.
+    This method uses a MongoDB aggregation pipeline.
+    Args:
+        mol (rdmol Object): query molecule
+        mol_collection (pymongo Collection): collection that has precomputed
+            Morgan Fingerprints and bit counts.
+        count_collection (pymongo Collection): popularity counts for Morgan
+            fingerprint bits for all entries in `mol_collection`
+        threshold: Tanimoto threshold for similarity
+    Returns
+        A list of MongoDB documents that fulfill `threshold`, including their
+        tanimoto scores.
+    """
+    results = []
+    qfp = list(
+        AllChem.GetMorganFingerprintAsBitVect(
+            mol, DEFAULT_MORGAN_RADIUS, nBits=DEFAULT_MORGAN_LEN
+        ).GetOnBits()
+    )
+    if threshold == 0:
+        for mol in mol_collection.find({}):
+            tanimoto = calc_tanimoto(qfp, mol["mfp"]["bits"])
+            mol["tanimoto"] = tanimoto
+            results.append(mol)
+        return results
+
+    qfp_count = len(qfp)
+    fp_min = int(math.ceil((threshold * qfp_count)))
+    try:
+        fp_max = int(qfp_count / threshold)
+    except ZeroDivisionError:
+        fp_max = float("inf")
+    req_common_count = qfp_count - fp_min + 1
+    if count_collection is not None:
+        req_common_bits = [
+            count["_id"]
+            for count in count_collection.find({"_id": {"$in": qfp}})
+            .sort("count", 1)
+            .limit(req_common_count)
+        ]
+    else:
+        req_common_bits = qfp[:req_common_count]
+    aggregate = [
+        {
+            "$match": {
+                "mfp.count": {"$gte": fp_min, "$lte": fp_max},
+                "mfp.bits": {"$in": req_common_bits},
+            }
+        },
+        {
+            "$set": {
+                "tanimoto": {
+                    "$let": {
+                        "vars": {
+                            "common": {
+                                "$size": {"$setIntersection": ["$mfp.bits", qfp]}
+                            }
+                        },
+                        "in": {
+                            "$divide": [
+                                "$$common",
+                                {
+                                    "$add": [
+                                        qfp_count,
+                                        {"$subtract": ["$mfp.count", "$$common"]},
+                                    ]
+                                },
+                            ]
+                        },
+                    }
+                },
+            }
+        },
+        {"$match": {"tanimoto": {"$gte": threshold}}},
+        {"$sort": {"tanimoto": -1}},
+    ]
+    response = mol_collection.aggregate(aggregate)
+
+    return response
+
 
 def calc_tanimoto(Na, Nb):
     """Calculates the Tanimoto similarity coefficient between two sets NA and NB."""
