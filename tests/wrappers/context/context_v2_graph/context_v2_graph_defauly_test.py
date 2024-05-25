dafdfc84f40d65a8b156c46e2020a34cc27462d1
Zhengkai Tu <ztu@mit.edu>
2024-05-25 14:44:16 -0400

    update make to remove adapter test by default; cluster and context refactored and standardized

--- tests/wrappers/context/context_v2_graph/context_v2_graph_defauly_test.py
+++ tests/wrappers/context/context_v2_graph/context_v2_graph_defauly_test.py
@@ -8,15 +8,15 @@ V2_HOST = os.environ.get("V2_HOST", "http://0.0.0.0")
 V2_PORT = os.environ.get("V2_PORT", "9100")
 
 
-class ClusterTest(unittest.TestCase):
-    """Test class for Cluster wrapper"""
+class ContextV2GraphTest(unittest.TestCase):
+    """Test class for Context Predict Graph wrapper"""
 
     @classmethod
     def setUpClass(cls) -> None:
         """This method is run once before all tests in this class."""
         cls.session = requests.Session()
         cls.base_url = f"{V2_HOST}:{V2_PORT}/api"
-        cls.module_url = f"{V2_HOST}:{V2_PORT}/api/cluster"
+        cls.module_url = f"{V2_HOST}:{V2_PORT}/api/context-recommender/v2/predict/GRAPH"
 
     def get_async_result(self, task_id: str, timeout: int = 20):
         """Retrieve celery task output"""
@@ -41,7 +41,7 @@ class ClusterTest(unittest.TestCase):
             return response
 
     def test_1(self):
-        case_file = "tests/wrappers/cluster/default_test_case_1.json"
+        case_file = "tests/wrappers/context/context_v2_graph/default_test_case_1.json"
         with open(case_file, "r") as f:
             data = json.load(f)
 
@@ -56,12 +56,10 @@ class ClusterTest(unittest.TestCase):
         ).json()
         response_async = self.get_async_result(
             task_id=task_id,
-            timeout=20
+            timeout=60
         )
         response_async = response_async["output"]
 
         for response in [response_sync, response_async]:
             self.assertEqual(response["status_code"], 200)
             self.assertIsInstance(response["result"], list)
-            self.assertIsInstance(response["result"][0], list)
-            self.assertIsInstance(response["result"][1], dict)
